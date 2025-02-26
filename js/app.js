f41c3b223267eeade01600355421a3d14f015467
andrean10 <andreanramadhan10@gmail.com>
2025-02-26 22:22:48 +0700

    Add favicon images and update project settings; clean up CSS and JS files

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
-/* Theme Name:  Kasy- Responsive Landing page template
-  File Description: Main JS file of the template
-*/
-
-
-
 //  Window scroll sticky class add
 function windowScroll() {
   const navbar = document.getElementById("navbar");
   if (
-      document.body.scrollTop >= 50 ||
-      document.documentElement.scrollTop >= 50
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
   ) {
-      navbar.classList.add("nav-sticky");
+    navbar.classList.add("nav-sticky");
   } else {
-      navbar.classList.remove("nav-sticky");
+    navbar.classList.remove("nav-sticky");
   }
 }
 
-window.addEventListener('scroll', (ev) => {
+window.addEventListener("scroll", (ev) => {
   ev.preventDefault();
   windowScroll();
-})
-
+});
 
 //
 /********************* scroll top js ************************/
@@ -51,67 +44,66 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-
-// 
+//
 // Typed Text animation (animation)
-// 
+//
 
 try {
   var TxtType = function (el, toRotate, period) {
-      this.toRotate = toRotate;
-      this.el = el;
-      this.loopNum = 0;
-      this.period = parseInt(period, 10) || 2000;
-      this.txt = '';
-      this.tick();
-      this.isDeleting = false;
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
   };
   TxtType.prototype.tick = function () {
-      var i = this.loopNum % this.toRotate.length;
-      var fullTxt = this.toRotate[i];
-      if (this.isDeleting) {
-          this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-          this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-      var that = this;
-      var delta = 200 - Math.random() * 100;
-      if (this.isDeleting) { delta /= 2; }
-      if (!this.isDeleting && this.txt === fullTxt) {
-          delta = this.period;
-          this.isDeleting = true;
-      } else if (this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.loopNum++;
-          delta = 500;
-      }
-      setTimeout(function () {
-          that.tick();
-      }, delta);
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+    setTimeout(function () {
+      that.tick();
+    }, delta);
   };
   function typewrite() {
-      if (toRotate === 'undefined') {
-          changeText()
+    if (toRotate === "undefined") {
+      changeText();
+    } else var elements = document.getElementsByClassName("typewrite");
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute("data-type");
+      var period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
       }
-      else
-          var elements = document.getElementsByClassName('typewrite');
-      for (var i = 0; i < elements.length; i++) {
-          var toRotate = elements[i].getAttribute('data-type');
-          var period = elements[i].getAttribute('data-period');
-          if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-          }
-      }
-      // INJECT CSS
-      var css = document.createElement("style");
-      css.type = "text/css";
-      css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid transparent}";
-      document.body.appendChild(css);
-  };
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML =
+      ".typewrite > .wrap { border-right: 0.08em solid transparent}";
+    document.body.appendChild(css);
+  }
   window.onload(typewrite());
-} catch(err) {
-}
+} catch (err) {}
 
 // home-1 slider
 var swiper = new Swiper(".mySwiper1", {
@@ -127,22 +119,21 @@ var swiper = new Swiper(".mySwiper1", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    00: {
-      slidesPerView: 1
+    0o0: {
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
-  }
+  },
 });
-
 
 // Animaten js
 
- AOS.init();
+AOS.init();
 
 //  home 8 swiper
- 
+
 var swiper = new Swiper(".mySwiper8", {
   slidesPerView: 1,
   spaceBetween: 30,
